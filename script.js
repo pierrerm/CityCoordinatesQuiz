2539748d466eefb6a261b04c9e17d0697675a9c4
Glitch (hello-webpage) <none>
2020-03-03 18:51:56 +0000

    🍶🚒 Checkpoint ./index.html:8556467/627 ./style.css:8556467/6522 ./script.js:8556467/434

--- script.js
+++ script.js
@@ -33,3 +33,14 @@ function addCityQuestion(){
     }
   );
 }
+
+function updateLatitude(){
+  var slider = document.getElementById("latitude");
+  var value = document.getElementById("latitudeValue");
+  value.innerHTML = "Choose a latitude: 0slider.value; // Display the default slider value
+
+  // Update the current slider value (each time you drag the slider handle)
+  slider.oninput = function() {
+    output.innerHTML = this.value;
+  }
+}
