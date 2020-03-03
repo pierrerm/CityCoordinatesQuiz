c38dc53faaf558bfe95b370507cd474e2acf7cc4
Glitch (hello-webpage) <none>
2020-03-03 18:28:37 +0000

    ⛲️👝 Checkpoint ./index.html:8556467/279 ./script.js:8556467/864

--- script.js
+++ script.js
@@ -15,11 +15,21 @@ function addCityQuestion(){
     "https://cdn.glitch.com/138c3ef8-a9f5-4f67-bcb8-162413e4f03c%2Fcities.json?v=1583211298978",
     function(citiesJson){
       console.log("Json file loaded: " + citiesJson.Cities.length + " entries.");
-      var div = document.getElementById('questionDiv');
+      var questionDiv = document.getElementById('questionDiv');
       index = Math.floor((Math.random() * citiesJson.Cities.length));
       var para = document.createElement("P");
       para.innerText = "Where is " + citiesJson.Cities[index].city + " located?";
-      div.appendChild(para); 
+      questionDiv.appendChild(para); 
+      var continentSelect = document.getElementById('continent');
+      var continents = ["Africa", "Asia", "Europe", "Oceania", "North America", "South America"];
+      for (var continent in continents){
+        var opt = document.createElement('option');
+        opt.value = continents[continent];
+        opt.innerHTML = continents[continent];
+        continentSelect.appendChild(opt);
+      }
+      var answerDiv = document.getElementById('answerDiv');
+      answerDiv.style.display = "block";
     }
   );
 }
