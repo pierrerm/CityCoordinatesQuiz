d03c6c966ce4aa7c7b2345c48bb6b3c703a64c1a
Glitch (hello-webpage) <none>
2020-03-03 17:54:18 +0000

    👙⛺️ Checkpoint ./script.js:8556467/2462 ./cities.json:8556467/7989112

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 /* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+   to your site with Javascript */
+
+var citiesJson;
+var city;
 
 function startQuiz() {
-  var json = $.getJSON(
-    "https://cdn.glitch.com/138c3ef8-a9f5-4f67-bcb8-162413e4f03c%2Fcities.json?v=1583211298978",
-    updatePage
-  );
-}
-
-function updatePage(json) {
   var elem = document.getElementById('startBtn');
   elem.parentNode.removeChild(elem);
-  var div = document.getElementById('questionDiv');
-  var index = Math.floor((Math.random() * json.Cities.length));
-  var para = document.createElement("P");
-  para.innerText = "Where is " + json.Cities[index].city + " located?";
-  div.appendChild(para);     
+  citiesJson = require("./cities.json");
+  addCityQuestion();
   return false;
+}
+
+function addCityQuestion(){
+  var div = document.getElementById('questionDiv');
+  var index = Math.floor((Math.random() * citiesJson.Cities.length));
+  var para = document.createElement("P");
+  para.innerText = "Where is " + citiesJson.Cities[index].city + " located?";
+  div.appendChild(para); 
 }
