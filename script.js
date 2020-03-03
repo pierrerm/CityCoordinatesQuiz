a1a186888dbb4c4c1f15f37c6d4a85b161dcf12d
Glitch (hello-webpage) <none>
2020-03-03 17:02:10 +0000

    💘💈 Checkpoint ./script.js:8556467/311 ./index.html:8556467/180

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+$.getJSON( "https://cdn.glitch.com/138c3ef8-a9f5-4f67-bcb8-162413e4f03c%2Fcities.json?v=1583211298978", function( json ) {
+    console.log( "JSON Data received, name is " + json.Cities[0].city);
+});
+
