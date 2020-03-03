abd74e553427b46bd58c79d187d9a8c46061144f
Glitch (hello-webpage) <none>
2020-03-03 17:36:50 +0000

    👱🌘 Checkpoint ./script.js:8556467/1565

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-$.getJSON( "https://cdn.glitch.com/138c3ef8-a9f5-4f67-bcb8-162413e4f03c%2Fcities.json?v=1583211298978", function( json ) {
-    console.log( "JSON Data received, name is " + json.Cities[0].city);
-});
-
 function startQuiz() {
-    var elem = document.getElementById('startBtn');
-    elem.parentNode.removeChild(elem);
-    var para = document.createElement("P");
-    para.innerText = "This is a paragraph";
-    document.body.appendChild(para);     
-    return false;
+  var json = $.getJSON(
+    "https://cdn.glitch.com/138c3ef8-a9f5-4f67-bcb8-162413e4f03c%2Fcities.json?v=1583211298978",
+    updatePage
+  );
+}
+
+function updatePage(json) {
+  var elem = document.getElementById('startBtn');
+  elem.parentNode.removeChild(elem);
+  var div = document.getElementById('questionDiv');
+  var index = Math.floor((Math.random() * json.Cities.length));
+  var para = document.createElement("P");
+  para.innerText = "Where is " + json.Cities[index].city + " located?";
+  div.appendChild(para);     
+  return false;
 }
